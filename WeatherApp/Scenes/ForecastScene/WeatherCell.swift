e4ee0e6df38222e5512666183449a43a89fadeac
csiAlexLu <129949657+csiAlexLu@users.noreply.github.com>
2023-04-05 14:06:50 +0300

    Refactor ui in WeatherCell.
    Refactor work with constraints and changed label to InfoLabel.

--- WeatherApp/Scenes/ForecastScene/WeatherCell.swift
+++ WeatherApp/Scenes/ForecastScene/WeatherCell.swift
@@ -17,56 +17,16 @@ class WeatherCell: UICollectionViewCell {
 
     static let reuseID = String(describing: WeatherCell.self)
 
-    enum Constants {
-        static let leadingInset: CGFloat = 20
-        static let trailingInset: CGFloat = -20
-        static let tempToImageInset: CGFloat = 10
-        static let dateToImageInset: CGFloat = -10
-        static let imageSide: CGFloat = 60
-
-        static let cornerRadius: CGFloat = 15
-        static let borderWidth: CGFloat = 3
-
-        static let tempFont = UIFont.systemFont(ofSize: 20.0, weight: .semibold)
-        static let dateFont = UIFont.systemFont(ofSize: 15.0, weight: .regular)
-
-        static let bgColor = UIColor.UI.purpleMain
-        static let borderColor = UIColor.UI.purpleBorder
-    }
-
-    private let dateLabel: UILabel = {
-        let label = UILabel()
-
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.textColor = .white
-        label.font = Constants.dateFont
-        label.textAlignment = .left
-        label.adjustsFontSizeToFitWidth = true
-
-        return label
-    }()
-
-    private let maxMinLabel: UILabel = {
-        let label = UILabel()
-
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.textColor = .white
-        label.font = Constants.tempFont
-        label.textAlignment = .right
-        label.numberOfLines = 2
-        label.adjustsFontSizeToFitWidth = true
-
-        return label
-    }()
+    private let dateLabel = InfoLabel(style: .small)
+    private let maxMinLabel = InfoLabel(style: .big)
 
     private let imageView = UIImageView()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
 
-        addViews()
-        addConstratints()
         prepareUI()
+        addConstratints()
     }
 
     required init?(coder: NSCoder) {
@@ -89,38 +49,49 @@ class WeatherCell: UICollectionViewCell {
     }
 
     private func prepareUI() {
-        backgroundColor = Constants.bgColor
-        imageView.translatesAutoresizingMaskIntoConstraints = false 
-    }
+        addSubviews([imageView, dateLabel, maxMinLabel])
 
-    private func addViews() {
-        addSubview(dateLabel)
-        addSubview(maxMinLabel)
-        addSubview(imageView)
+        backgroundColor = Constants.bgColor
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        dateLabel.textAlignment = .left
+        maxMinLabel.textAlignment = .right
+        maxMinLabel.numberOfLines = 2
     }
 
     private func addConstratints() {
-        NSLayoutConstraint.activate([
-            dateLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Constants.leadingInset),
-            dateLabel.topAnchor.constraint(equalTo: self.topAnchor),
-            dateLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-            dateLabel.trailingAnchor.constraint(equalTo: imageView.leadingAnchor, constant: Constants.dateToImageInset),
+        dateLabel
+            .pin(edges: [.left, .bottom, .top], to: self, with: Constants.dateLabelInsets)
 
-            maxMinLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: Constants.trailingInset),
-            maxMinLabel.topAnchor.constraint(equalTo: self.topAnchor),
-            maxMinLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-            maxMinLabel.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: Constants.tempToImageInset),
+        imageView
+            .center()
+            .height(Constants.imageSide)
+            .width(Constants.imageSide)
+            .toRight(of: dateLabel, with: Constants.dateToImageInset)
 
-            imageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            imageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            imageView.heightAnchor.constraint(equalToConstant: Constants.imageSide),
-            imageView.widthAnchor.constraint(equalToConstant: Constants.imageSide)
-        ])
+        maxMinLabel
+            .pin(edges: [.right, .bottom, .top], to: self, with: Constants.maxMinLabelLabelInsets)
+            .toRight(of: imageView, with: Constants.dateToImageInset)
     }
 
     func setupCell(uiModel: WeatherCellUIModel) {
         dateLabel.text = uiModel.date
         maxMinLabel.text = uiModel.maxMinTemp
         imageView.downloaded(from: uiModel.image)
+    }
+}
+
+extension WeatherCell {
+    enum Constants {
+
+        static let dateLabelInsets = UIEdgeInsets(top: 0, left: 20, bottom: 0)
+        static let maxMinLabelLabelInsets = UIEdgeInsets(top: 0, bottom: 0, right: 20)
+        static let dateToImageInset: CGFloat = 10
+        static let imageSide: CGFloat = 60
+
+        static let cornerRadius: CGFloat = 15
+        static let borderWidth: CGFloat = 3
+
+        static let bgColor = UIColor.UI.purpleMain
+        static let borderColor = UIColor.UI.purpleBorder
     }
 }
