3f2a08f1968546b05a1363264d71c84ab8b83e4c
csiAlexLu <129949657+csiAlexLu@users.noreply.github.com>
2023-04-05 14:06:50 +0300

    Add UIImage extension for image download.

---
+++ WeatherApp/Extensions/UIImage+Extensions.swift
@@ -0,0 +1,30 @@
+//
+//  UIImage+Extensions.swift
+//  WeatherApp
+//
+//  Created by Alexander Luchenok on 4/3/23.
+//
+
+import UIKit
+
+extension UIImageView {
+    func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { [weak self] in
+                self?.image = image
+            }
+        }.resume()
+    }
+
+    func downloaded(from link: String, contentMode mode: ContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloaded(from: url, contentMode: mode)
+    }
+}
