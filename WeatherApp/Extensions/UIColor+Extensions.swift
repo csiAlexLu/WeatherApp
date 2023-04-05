acfe8cfa883b5881e3e0eee7ae63a09ecd8ee801
csiAlexLu <129949657+csiAlexLu@users.noreply.github.com>
2023-04-05 14:06:50 +0300

    Add UIColor extension.
    Add an extension for wrapping colors from assets and more convenient use.

---
+++ WeatherApp/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Extensions.swift
+//  WeatherApp
+//
+//  Created by Alexander Luchenok on 3/31/23.
+//
+
+import UIKit
+
+extension UIColor {
+    enum UI {
+        static let purpleMain = UIColor(named: "purpleMain")!
+        static let purpleBorder = UIColor(named: "purpleBorder")!
+    }
+}
