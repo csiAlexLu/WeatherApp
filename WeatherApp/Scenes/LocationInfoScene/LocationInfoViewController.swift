51940873c0c14e51724e5460c3c5acafa82a5188
csiAlexLu <129949657+csiAlexLu@users.noreply.github.com>
2023-04-05 14:06:50 +0300

    Refactor ui in LocationInfoViewController.
    Refactor work with constraints and changed label to InfoLabel.

--- WeatherApp/Scenes/LocationInfoScene/LocationInfoViewController.swift
+++ WeatherApp/Scenes/LocationInfoScene/LocationInfoViewController.swift
@@ -10,71 +10,10 @@ import Combine
 
 class LocationInfoViewController: UIViewController, BaseViewController {
 
-    enum Constants {
-        static let leadingInset: CGFloat = 20
-        static let trailingInset: CGFloat = -20
-        static let verticalInset: CGFloat = 15
-        static let mainInfoHeight: CGFloat = 30
-        static let additionalInfoHeight: CGFloat = 20
-
-        static let cornerRadius: CGFloat = 15
-        static let borderWidth: CGFloat = 3
-
-
-        static let mainInfoFont = UIFont.systemFont(ofSize: 30.0, weight: .semibold)
-        static let additionalInfoFont = UIFont.systemFont(ofSize: 15.0, weight: .regular)
-
-        static let bgColor = UIColor.UI.purpleMain
-        static let borderColor = UIColor.UI.purpleBorder
-    }
-
-    private let citylabel: UILabel = {
-        let label = UILabel()
-
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.textColor = .white
-        label.font = Constants.mainInfoFont
-        label.textAlignment = .center
-        label.adjustsFontSizeToFitWidth = true
-
-        return label
-    }()
-
-    private let countylabel: UILabel = {
-        let label = UILabel()
-
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.textColor = .white
-        label.font = Constants.additionalInfoFont
-        label.textAlignment = .center
-        label.adjustsFontSizeToFitWidth = true
-
-        return label
-    }()
-
-    private let timelabel: UILabel = {
-        let label = UILabel()
-
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.textColor = .white
-        label.font = Constants.mainInfoFont
-        label.textAlignment = .center
-        label.adjustsFontSizeToFitWidth = true
-
-        return label
-    }()
-
-    private let datelabel: UILabel = {
-        let label = UILabel()
-
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.textColor = .white
-        label.font = Constants.additionalInfoFont
-        label.textAlignment = .center
-        label.adjustsFontSizeToFitWidth = true
-
-        return label
-    }()
+    private let citylabel = InfoLabel(style: .big)
+    private let countylabel = InfoLabel(style: .small)
+    private let timelabel = InfoLabel(style: .big)
+    private let datelabel = InfoLabel(style: .small)
 
     var viewModel: LocationInfoViewModel
     private var subscriptions: Set<AnyCancellable> = []
@@ -101,44 +40,36 @@ class LocationInfoViewController: UIViewController, BaseViewController {
         super.viewDidLoad()
 
         prepareUI()
-        addViews()
         addConstratints()
         addObservers()
     }
 
     private func prepareUI() {
+        let labels = [citylabel, countylabel, timelabel, datelabel]
+        view.addSubviews(labels)
         view.backgroundColor = Constants.bgColor
-    }
-
-    private func addViews() {
-        view.addSubview(citylabel)
-        view.addSubview(countylabel)
-        view.addSubview(timelabel)
-        view.addSubview(datelabel)
+        labels.forEach { $0.textAlignment = .center }
     }
 
     private func addConstratints() {
-        NSLayoutConstraint.activate([
-            citylabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.leadingInset),
-            citylabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: Constants.trailingInset),
-            citylabel.topAnchor.constraint(equalTo: view.topAnchor, constant: Constants.verticalInset),
-            citylabel.heightAnchor.constraint(equalToConstant: Constants.mainInfoHeight),
+        citylabel
+            .pin(edges: [.left, .right, .top], to: self.view, with: Constants.cityLabelInsets)
+            .height(Constants.mainInfoHeight)
 
-            countylabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.leadingInset),
-            countylabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: Constants.trailingInset),
-            countylabel.topAnchor.constraint(equalTo: citylabel.bottomAnchor),
-            countylabel.heightAnchor.constraint(equalToConstant: Constants.additionalInfoHeight),
+        countylabel
+            .toBottom(of: citylabel)
+            .pin(edges: [.left, .right], to: self.view, with: Constants.countryLabelInsets)
+            .height(Constants.additionalInfoHeight)
 
-            timelabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.leadingInset),
-            timelabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: Constants.trailingInset),
-            timelabel.topAnchor.constraint(equalTo: countylabel.bottomAnchor, constant: Constants.verticalInset),
-            timelabel.heightAnchor.constraint(equalToConstant: Constants.mainInfoHeight),
+        timelabel
+            .toBottom(of: countylabel, with: Constants.verticalInset)
+            .pin(edges: [.left, .right], to: self.view, with: Constants.countryLabelInsets)
+            .height(Constants.mainInfoHeight)
 
-            datelabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.leadingInset),
-            datelabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: Constants.trailingInset),
-            datelabel.topAnchor.constraint(equalTo: timelabel.bottomAnchor),
-            datelabel.heightAnchor.constraint(equalToConstant: Constants.additionalInfoHeight),
-        ])
+        datelabel
+            .toBottom(of: timelabel)
+            .pin(edges: [.left, .right], to: self.view, with: Constants.countryLabelInsets)
+            .height(Constants.additionalInfoHeight)
     }
 
     private func addObservers() {
@@ -168,5 +99,19 @@ class LocationInfoViewController: UIViewController, BaseViewController {
 
     func setWeather(_ weather: Weather?) {
         viewModel.setWeather(weather)
+    }
+}
+
+extension LocationInfoViewController {
+    enum Constants {
+        static let cityLabelInsets = UIEdgeInsets(top: 15, left: 20, right: 20)
+        static let countryLabelInsets = UIEdgeInsets(left: 20, right: 20)
+        static let verticalInset: CGFloat = 15
+        static let mainInfoHeight: CGFloat = 30
+        static let additionalInfoHeight: CGFloat = 20
+        static let cornerRadius: CGFloat = 15
+        static let borderWidth: CGFloat = 3
+        static let bgColor = UIColor.UI.purpleMain
+        static let borderColor = UIColor.UI.purpleBorder
     }
 }
